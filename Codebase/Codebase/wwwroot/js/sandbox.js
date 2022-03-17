9ae5aea78dcb3974c4a5f87c750b46a5c840be92
schlagmt <mtschlager@gmail.com>
2022-03-17 14:25:40 -0400

    wip

--- Codebase/Codebase/wwwroot/js/sandbox.js
+++ Codebase/Codebase/wwwroot/js/sandbox.js
@@ -19,9 +19,17 @@
 			var textarea_editor = document.getElementById("textarea_editor");
 			textarea_editor.value = v.snippet.code;
 
+			var lang = null;
+			if (v.snippet.language === "c" || v.snippet.language === "c#" || v.snippet.language === "c++") {
+				lang = "clike";
+			}
+			else {
+				lang = v.snippet.language;
+			}
+
 			v.editor = CodeMirror.fromTextArea(textarea_editor, {
 				tabSize: 4,
-				mode: v.snippet.language,
+				mode: lang,
 				theme: '3024-night',
 				lineNumbers: true,
 				styleActiveSelected: true,
@@ -91,7 +99,59 @@
 		compile: function () {
 			let v = this;
 
+			var langcode = null;
+			var versionindex = null;
 
+			if (v.snippet.language === "c") {
+				langcode = "c";
+				versionindex = "5";
+			} else if (v.snippet.language === "c++") {
+				langcode = "cpp17";
+				versionindex = "1";
+			} else if(v.snippet.language === "php") {
+				langcode = "php";
+				versionindex = "4";
+			} else if (v.snippet.language === "perl") {
+				langcode = "perl";
+				versionindex = "4";
+			} else if (v.snippet.language === "python") {
+				langcode = "python3";
+				versionindex = "4";
+			} else if (v.snippet.language === "ruby") {
+				langcode = "ruby";
+				versionindex = "4";
+			} else if (v.snippet.language === "go") {
+				langcode = "go";
+				versionindex = "4";
+			} else if (v.snippet.language === "c#") {
+				langcode = "csharp";
+				versionindex = "4";
+			} else if (v.snippet.language === "r") {
+				langcode = "r";
+				versionindex = "4";
+			} else {
+				langcode = "clojure";
+				versionindex = "3";
+			}
+
+			$.ajax({
+				url: "https://api.jdoodle.com/v1/execute",
+				contentType: "application/json; charset=utf-8",
+				data: {
+					clientId: "a7c32c59a02db14c68f27b026c75e6b6",
+					clientSecret: "fac2276dcb1d21eb57450fee25566108916fc6611e9e21ab1fc10509f84a2da6",
+					script: v.editor.getValue(),
+					language: langcode,
+					versionIndex: versionindex
+				},
+				type: "POST",
+				success: function (data) {
+					v.output.setValue(data.output)
+				},
+				error: function (error) {
+					v.output.setValue(error.error);
+				}
+			});
         },
 
 	}
